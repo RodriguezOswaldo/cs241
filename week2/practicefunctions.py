ff4044f9035ba5514ff15611808a15061e80d208
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-04-28 02:21:13 -0400

    functions, while and conditionals practice week 2

--- week2/practicefunctions.py
+++ week2/practicefunctions.py
@@ -1,22 +1,26 @@
-import sys
-
 def prompt_number():
-
-    number = 0
+    number = int(input("Enter a positive number: "))
     while number <= 0:
-        number = int(input('Enter a positive number: '))
-        sys.exit(0)
+        print("Invalid entry. The number must be positive.")
+        number = int(input("Enter a positive number: "))
+    if number > 0:
+        print('')
+
     return number
 
-def compute_sum(numberOne, numberTwo, numberThree):
 
-    allTogether = numberOne + numberTwo + numberThree
-    return allTogether
+def compute_sum(number_one, number_two, number_three):
+    all_together = number_one + number_two + number_three
+    return all_together
 
 
-numberOne = prompt_number()
-numberTwo = prompt_number()
-numberThree = prompt_number()
-sumup_number = compute_sum(numberOne, numberTwo, numberThree)
+def main():
+    number_one = prompt_number()
+    number_two = prompt_number()
+    number_three = prompt_number()
+    total = compute_sum(number_one, number_two, number_three)
+    print('The sum is: ' + str(total))
 
-print('\nThe sum is ' + str(sumup_number))
+
+if __name__ == "__main__":
+    main()
