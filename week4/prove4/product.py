74a74bf99cd46f83c258c7975b94942418086248
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-05-16 01:32:33 -0400

    the three classes are working as expected

--- week4/prove4/product.py
+++ week4/prove4/product.py
@@ -48,22 +48,40 @@ class Customer:
     def __init__(self):
         self.id = 0
         self.name = ''
-        self.order = Order()
+        self.orders = []
 
-    def get_order_count(self, ):
-        pass
+    def get_order_count(self):
+        order_count = 0
+        for my_order in self.orders:
+            order_count += my_order
+        return order_count
 
-    def get_total(self, ):
+    def get_total(self):
         pass
 
     def add_order(self, order):
-        pass
+        self.orders.append(order)
 
-    def display_summary(self, ):
-        pass
+    def display_summary(self):
+        total_order = 0
+        add = 0
+        for order in self.orders:
+            total_order += order.get_total()
+            add += 1
 
-    def display_receipts(self, ):
-        pass
+        print(f'Summary for customer \'{self.id}\'')
+        print(f'Name: {self.name}')
+        print(f'Order: {add}')
+        print(f'Total: ${round(total_order,2)}')
+
+    def display_receipts(self):
+        print(f'Detailed receipts for customer \'{self.id}\':')
+        print(f'Name: {self.name}')
+        print()
+
+        for o in self.orders:
+            o.display_receipt()
+            print()
 
 
 def main():
