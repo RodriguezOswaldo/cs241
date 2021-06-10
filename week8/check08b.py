8a9a7564e61335858e0243798e2e84b79120bdc7
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-06-09 21:35:19 -0400

    week 8 - getter & setter using @property and property

--- week8/check08b.py
+++ week8/check08b.py
@@ -1,65 +1,68 @@
 class GPA:
     def __init__(self):
-        self.gpa = 0.0
+        self._gpa = 0.0
 
-    def get_gpa(self):
-        return self.gpa
+    def _get_gpa(self):
+        return self._gpa
 
-    def set_gpa(self, value):
+    def _set_gpa(self, value):
         if value < 0.0:
-            self.gpa = 0.0
+            self._gpa = 0.0
         elif value > 4.0:
-            self.gpa = 4.0
+            self._gpa = 4.0
         else:
-            self.gpa = value
-
-    def get_letter(self):
-        if self.gpa < 0.99:
+            self._gpa = value
+    @property
+    def letter(self):
+        if self._gpa < 0.99:
             return "F"
-        elif self.gpa <= 1.99:
+        elif self._gpa <= 1.99:
             return "D"
-        elif self.gpa <= 2.99:
+        elif self._gpa <= 2.99:
             return "C"
-        elif self.gpa <= 3.99:
+        elif self._gpa <= 3.99:
             return "B"
         else:
             return "A"
-
-    def set_letter(self, letter):
+    @letter.setter
+    def letter(self, letter):
         if letter == "F":
-            self.gpa = 0.0
+            self._gpa = 0.0
         elif letter == "D":
-            self.gpa = 1.0
+            self._gpa = 1.0
         elif letter == "C":
-            self.gpa = 2.0
+            self._gpa = 2.0
         elif letter == "B":
-            self.gpa = 3.0
+            self._gpa = 3.0
         elif letter == "A":
-            self.gpa = 4.0
+            self._gpa = 4.0
+
+    gpa = property(_get_gpa, _set_gpa)
+    letter = letter
 
 
 def main():
     student = GPA()
 
     print("Initial values:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
-    value = float(input("Enter a new GPA: "))
+    student.gpa = float(input("Enter a new GPA: "))
 
-    student.set_gpa(value)
+    student.gpa
 
     print("After setting value:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
-    letter = input("Enter a new letter: ")
+    student.letter = input("Enter a new letter: ")
 
-    student.set_letter(letter)
+    student.letter
 
     print("After setting letter:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
 
 if __name__ == "__main__":
