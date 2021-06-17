668f46a8dfcdaa9161544d4e1652b7e2761384c2
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-06-17 15:18:15 -0400

    week 9 - handling exceptions - Team

---
+++ week9/team_exceptions.py
@@ -0,0 +1,23 @@
+class BalanceError(Exception):
+    pass
+
+class OutOfChecksError(Exception):
+    pass
+class CheckingAccount:
+    def __init__(self, starting_balance, num_checks):
+        self.balance = starting_balance
+        self.check_count = num_checks
+
+    def deposit(self, amount):
+        pass
+
+    def write_check(self, amount):
+        pass
+
+    def display(self):
+        pass
+
+    def apply_for_credit(self):
+        pass
+
+
