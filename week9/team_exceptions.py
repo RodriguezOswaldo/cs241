287253082bf07589d7707562ff5a1e8b814dfe33
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-06-17 16:23:58 -0400

    adding the main and commands function

--- week9/team_exceptions.py
+++ week9/team_exceptions.py
@@ -24,3 +24,48 @@ class CheckingAccount:
         pass
 
 
+def display_menu():
+    """
+    Displays the available commands.
+    """
+    print()
+    print("Commands:")
+    print("  quit - Quit")
+    print("  new - Create new account")
+    print("  display - Display account information")
+    print("  deposit - Desposit money")
+    print("  check - Write a check")
+
+
+def main():
+    """
+    Used to test the CheckingAccount class.
+    """
+    acc = None
+    command = ""
+
+    while command != "quit":
+        display_menu()
+        command = input("Enter a command: ")
+
+        if command == "new":
+            balance = float(input("Starting balance: "))
+            num_checks = int(input("Numbers of checks: "))
+
+            acc = CheckingAccount(balance, num_checks)
+        elif command == "display":
+            acc.display()
+        elif command == "deposit":
+            amount = float(input("Amount: "))
+            acc.deposit(amount)
+        elif command == "check":
+            amount = float(input("Amount: "))
+            acc.write_check(amount)
+        elif command == "credit":
+            amount = float(input("Amount: "))
+            acc.apply_for_credit(amount)
+
+
+if __name__ == "__main__":
+    main()
+
