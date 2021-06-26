b614f30f9c934af5e20157f7866d481daae49478
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-06-25 22:45:53 -0400

    practicing with list's methods'

--- week10/lists.py
+++ week10/lists.py
@@ -1,5 +1,25 @@
 fish = ['barracuda', 'cod', 'devil ray', 'eel']
 
+# insert item at the end of the list
 fish.append('flounder')
 
+print(fish)
+
+# insert item at specified index
+fish.insert(0, 'owner')
+
+print(fish)
+
+more_fish = ['goby', 'herring', 'ide', 'kissing gourami']
+
+# Add a second lists to an existing lists
+fish.extend(more_fish)
+print(fish)
+
+# Removing one item from the list
+fish.remove('kissing gourami')
+print(fish)
+
+# remove the last item in the list or the one in the specified index
+print(fish.pop(3))
 print(fish)
