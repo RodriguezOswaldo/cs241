c26d19faf8883f1844ea1ed0827d2def280d4849
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-07-02 23:24:46 -0400

    more list comprehension - data structure

--- week11/comprehension.py
+++ week11/comprehension.py
@@ -27,3 +27,24 @@ print(new_list)
 second_list = [n**2 for n in range(10) if n % 2 == 0]
 print(second_list)
 
+"""
+ More practice in list comprehensions
+"""
+
+squares = [n**3 for n in range(100)]
+print(squares)
+
+
+"""
+numbers divisibles by 5 or 7
+"""
+mi_n = 1
+# divisibles = [n for n in range(0, 99) if n % 5 == 0]
+while mi_n != 0:
+    mi_n = int(input('what number do you want to divide for?: \n'))
+    if mi_n == 5:
+        divisibles = [mi_n for mi_n in range(0, 99) if mi_n % 5 == 0]
+        print(divisibles)
+    else:
+        divisibles_two = [mi_n for mi_n in range(0,99) if mi_n % 7 == 0]
+        print(divisibles_two)
