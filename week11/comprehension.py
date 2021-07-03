d0e4710c8dccaec8e658a3df82014ebebab2a24b
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-07-02 23:05:06 -0400

    list comprehension - data structure

---
+++ week11/comprehension.py
@@ -0,0 +1,19 @@
+for x in range(10):
+    x**2
+s = [x**2 for x in range(10)]
+
+for i in range(13):
+    2**i
+v = [2**i for i in range(13)]
+
+print(s)
+print(v)
+
+S = [1, 2, 3, 4, 5, 6]
+M = [x for x in S if x % 2 == 0]
+print(M)
+
+
+d = [x**3 for x in range(10)]
+print(d)
+
