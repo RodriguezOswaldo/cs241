a11b2ccf517d863e72b3d1ba956f32e2539c0bb3
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-07-07 16:49:37 -0400

    trying to find the mean

--- week12/checka.py
+++ week12/checka.py
@@ -2,5 +2,10 @@ import pandas
 import matplotlib.pyplot as plt
 
 path = '/Users/ownos/Programming/cs241/week9/census.csv'
-_data = pandas.read_csv(path)
-print(_data.tail())
+data = pandas.read_csv(path)
+print(data.tail())
+# mean = _data.income.mean()
+# max = data[0].max()
+col = data.columns
+age = data[0].max()
+print(age)
